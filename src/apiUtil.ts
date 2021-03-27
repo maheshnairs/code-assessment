aed6cdcd844059b38e2033839a3cd627750fb42b
Mahesh Nair <msn0989@gmail.com>
2021-03-27 15:31:44 +0530

    Add files via upload

--- src/apiUtil.ts
+++ src/apiUtil.ts
@@ -1,8 +1,22 @@
-export async function getAllCountries(){
-   return await fetch('https://restcountries.eu/rest/v2/all')
-  .then(response => response.json())
+export async function getAllCountries() {
+  return await fetch('https://restcountries.eu/rest/v2/all')
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Error Connecting Server');
+      }
+    })
+    .catch(error => console.log(error));
 }
-export async function getCountryDetails(name: String){
+export async function getCountryDetails(name: String) {
   return await fetch(`https://restcountries.eu/rest/v2/name/${name}?fullText=true`)
- .then(response => response.json())
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Error Connecting Server');
+      }
+    })
+    .catch(error => console.log(error));
 }
